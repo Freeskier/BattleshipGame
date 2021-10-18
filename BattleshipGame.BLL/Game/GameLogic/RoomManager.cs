be01c2b15d8ea960329d17654a9310a21d9bea6c
Emil Mąka <emil.maka@student.wat.edu.pl>
2021-10-18 15:39:53 +0200

    backend dev continuation

--- BattleshipGame.BLL/Game/GameLogic/RoomManager.cs
+++ BattleshipGame.BLL/Game/GameLogic/RoomManager.cs
@@ -1,9 +1,16 @@
+using System.Collections.Generic;
 using BattleshipGame.BLL.Game.GameLogic.Interfaces;
 
 namespace BattleshipGame.BLL.Game.GameLogic
 {
     public class RoomManager : IRoomManager
     {
-        
+        public Dictionary<string, string> ConnectedUsers {get; set;}
+
+        public RoomManager()
+        {
+            ConnectedUsers = new Dictionary<string, string>();
+        }
+
     }
 }
--- BattleshipGame.BLL/Hubs/GameHub.cs
+++ BattleshipGame.BLL/Hubs/GameHub.cs
@@ -1,9 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BattleshipGame.BLL.Game.GameLogic.Interfaces;
+using BattleshipGame.BLL.Hubs.Models;
+using BattleshipGame.DAL.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace BattleshipGame.BLL.Hubs
 {
+    [Authorize]
     public class GameHub : Hub<IGameHub>
     {
+  
+        public static IRoomManager _roomManager;
+        
+
+        public GameHub(IRoomManager roomManager)
+        {
+            _roomManager = roomManager;
+        }
+
+        public async Task ChallengeUser(ChallengeUserModel model)
+        {
+        }
+        public async Task ChallengeUserCallback(ChallengeUserModel model)
+        {
+
+        }
+        public override async Task OnConnectedAsync()
+        {
+            lock(_roomManager)
+            {
+                _roomManager.ConnectedUsers.Add(Context.ConnectionId, Context.User.Identity.Name);
+            }
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            lock(_roomManager)
+            {
+                _roomManager.ConnectedUsers.Remove(Context.ConnectionId);
+            }
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        
 
     }
 }
