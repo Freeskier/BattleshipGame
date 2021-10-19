2e844d7c1dbc4b2fab8d9ae97e3cb9b8360e5425
Emil Mąka <emil.maka@student.wat.edu.pl>
2021-10-20 00:30:06 +0200

    Adding game logic

--- BattleshipGame.WEB/battleshipgame/src/Components/GamePanel/GamePanel.js
+++ BattleshipGame.WEB/battleshipgame/src/Components/GamePanel/GamePanel.js
@@ -1,11 +1,90 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Board from '../Board/Board';
 import Stats from '../Board/Stats';
 import Chat from '../Chat/Chat';
 import './GamePanel.css'
+import { HubConnectionBuilder } from '@microsoft/signalr';
+import { toast } from 'react-toastify';
+import {useHistory} from 'react-router-dom'
+import ChallengeModal from '../Chat/ChallengeModal';
+
 
 function GamePanel() {
+    let history = useHistory();
 
+    const [connection, setConnection] = useState(null);
+    const [myBoard, setMyBoard] = useState([]);
+    const [enemyBoard, setnemyBoard] = useState([]);
+    const [openResponseModal, setOpenResponseModal] = useState(false);
+    const [challengingUser, setChallengingUser] = useState('');
+
+    useEffect(() => {
+        const newConnection = new HubConnectionBuilder()
+            .withUrl('http://localhost:5000/hub/game', { accessTokenFactory: () => localStorage.getItem('token') })
+            .withAutomaticReconnect()
+            .build();
+        setConnection(newConnection);
+    }, []);
+
+    useEffect(() => {
+        if (connection) {
+            connection.start()
+            .then(result => {    
+                connection.on('ChallengeUserCallback', challenge => {
+                    setChallengingUser(challenge.fromUser);
+                    setOpenResponseModal(true);
+                    console.log("ASD");
+                });
+
+            })
+                .catch(e => {
+                    if(e.statusCode === 401)
+                    {
+                        toast.error('Authentication failed.')
+                        history.push('/');
+                        localStorage.clear();
+                    }
+                }
+            );
+        }
+    }, [connection]);
+
+    const sendChallenge = async (user) => {
+        if(localStorage.getItem('login') === user) {
+            toast.warn('You cannot send challenge to yourself.');
+            return;
+        }
+
+        const challenge = {
+            toUser: user,
+            fromUser: localStorage.getItem('login'),
+        };
+
+        if (connection) {
+            if (connection.connectionStarted) {
+                try {
+                    await connection.send('SendChallenge', challenge);
+                }
+                catch(e) {
+                    toast.error(e);
+                }
+        }}}
+
+    const sendResponse = async () => {
+        const challenge = {
+            toUser: challengingUser,
+            fromUser: localStorage.getItem('login'),
+        };
+
+        if (connection) {
+            if (connection.connectionStarted) {
+                try {
+                    await connection.send('AcceptChallenge', challenge);
+                }
+                catch(e) {
+                    toast.error(e);
+                }
+        }}}
 
     return ( 
         <div className='game-panel-container'>
@@ -26,8 +105,11 @@ function GamePanel() {
                         </div>
                     </div>
                 </div>
-                <Chat/>
+                <Chat sendChallenge={sendChallenge} />
             </div>
+            <ChallengeModal isOpen={openResponseModal} setOpen={setOpenResponseModal} 
+                user={challengingUser} sendChallenge={sendResponse}
+                isItResponse={true}/>
         </div>
      );
 }
